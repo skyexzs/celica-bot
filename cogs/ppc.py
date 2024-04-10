f94e6eefd0071a633f66d6d43bcf436af65dbb65
Skyexzs <willem_tranku@hotmail.com>
2024-04-10 14:33:57 +0700

    Removed SS from ppc commands
    Reason due to format error

--- cogs/ppc.py
+++ cogs/ppc.py
@@ -108,25 +108,25 @@ class Boss_Button_View(discord.ui.View):
         else:
             self.splus_btn = Boss_Button_UI('<:SSSPlus:1041604127773442058>', emb=default_emb, id='splus')"""
         
-        self.ss_btn = Boss_Button_UI('<:SS:1041604124942270514>', emb=ss_emb, id='ss', disabled=True)
-        self.sss_btn = Boss_Button_UI('<:SSS:1085394905242808373>', emb=sss_emb, id='sss')
+        # self.ss_btn = Boss_Button_UI('<:SS:1041604124942270514>', emb=ss_emb, id='ss', disabled=True)
+        self.sss_btn = Boss_Button_UI('<:SSS:1085394905242808373>', emb=sss_emb, id='sss', disabled=True)
         self.splus_btn = Boss_Button_UI('<:SSSPlus:1041604127773442058>', emb=splus_emb, id='splus')
-        self.add_item(self.ss_btn)
+        # self.add_item(self.ss_btn)
         self.add_item(self.sss_btn)
         self.add_item(self.splus_btn)
         self.timeout = timeout
     
     async def callback(self, interaction: discord.Interaction, btn_id: str, emb: discord.Embed):
-        if btn_id == 'ss':
-            self.ss_btn.disabled = True
-            self.sss_btn.disabled = False
-            self.splus_btn.disabled = False
-        elif btn_id == "sss":
-            self.ss_btn.disabled = False
+        # if btn_id == 'ss':
+        #     self.ss_btn.disabled = True
+        #     self.sss_btn.disabled = False
+        #     self.splus_btn.disabled = False
+        if btn_id == "sss":
+            # self.ss_btn.disabled = False
             self.sss_btn.disabled = True
             self.splus_btn.disabled = False
         else:
-            self.ss_btn.disabled = False
+            # self.ss_btn.disabled = False
             self.sss_btn.disabled = False
             self.splus_btn.disabled = True
         await interaction.response.edit_message(embed=emb, view=self)
@@ -228,15 +228,15 @@ class PPC(commands.Cog):
         
         # ALIASES 1st = SS, 2nd = S+, 3rd = SSS
         # These are differentiated so it will still run if one fails
-        try:
-            for i in range(len(dropdown.values)):
-                # Get from SS scores
-                alias = aliases[i][0]
-                ssws = self.ss_sh.worksheet(alias)
+        # try:
+        #     for i in range(len(dropdown.values)):
+        #         # Get from SS scores
+        #         alias = aliases[i][0]
+        #         ssws = self.ss_sh.worksheet(alias)
 
-                raw_ss_total += int(ssws.acell(SS_TOTAL_SCORE_CELL).value)
-        except :
-            raw_ss_total = 0
+        #         raw_ss_total += int(ssws.acell(SS_TOTAL_SCORE_CELL).value)
+        # except :
+        #     raw_ss_total = 0
         try:
             for i in range(len(dropdown.values)):
                 # Get from SSS scores
@@ -263,7 +263,7 @@ class PPC(commands.Cog):
 
         if interaction.guild.id == 887647011904557068:
             err = False
-            if raw_whale_total == 0 or raw_sss_total == 0 or raw_ss_total == 0:
+            if raw_whale_total == 0 or raw_sss_total == 0: # or raw_ss_total == 0:
                 err = True
             text = ''
             if raw_whale_total != 0:
@@ -274,16 +274,16 @@ class PPC(commands.Cog):
                 # SSS max scores rounded down to nearest 10k - 5000 (for SSS)
                 legend = (math.floor(raw_sss_total / 10000) * 10000) - 5000
                 text += f'<:Hero:1097168015386234930> <@&1031387046016720908>: {legend}\n'
-            if raw_ss_total != 0:
-                # SS max scores rounded down to nearest 10k and -5k (for SS)
-                conqueror = (math.floor(raw_ss_total / 10000) * 10000) - 5000
-                text += f'<:GrandMaster:1097168010671820860> <@&977900757972029461>: {conqueror}'
+            # if raw_ss_total != 0:
+            #     # SS max scores rounded down to nearest 10k and -5k (for SS)
+            #     conqueror = (math.floor(raw_ss_total / 10000) * 10000) - 5000
+            #     text += f'<:GrandMaster:1097168010671820860> <@&977900757972029461>: {conqueror}'
             if err is False:
                 emb.add_field(name='Required scores for roles:', value=text, inline=False)
             else:
                 emb.add_field(name='Required scores for roles:', value="Invalid calculation! Please contact the mods to fix the results.", inline=False)
 
-        emb.add_field(name='Max achievable scores:', value=f'SSS+ spreadsheet: **{raw_whale_total}**\nSSS spreadsheet: **{raw_sss_total}**\nSS spreadsheet: **{raw_ss_total}**')
+        emb.add_field(name='Max achievable scores:', value=f'SSS+ spreadsheet: **{raw_whale_total}**\nSSS spreadsheet: **{raw_sss_total}**') #\nSS spreadsheet: **{raw_ss_total}**')
 
         success = utl.make_embed(desc="Success!", color=discord.Colour.green())
         await interaction.edit_original_response(embed=success, view=None)
@@ -337,7 +337,7 @@ class PPC(commands.Cog):
         skip = False
         if self.boss_embed_cache.get(boss, None) is not None:
             if (datetime.datetime.now() - self.boss_time_created_cache[boss]).total_seconds() < 900:
-                ss_emb = self.boss_embed_cache[boss][0]
+                # ss_emb = self.boss_embed_cache[boss][0]
                 sss_emb = self.boss_embed_cache[boss][1]
                 emb = self.boss_embed_cache[boss][2]
                 skip = True
@@ -345,52 +345,52 @@ class PPC(commands.Cog):
         if not skip:
             diff = ['Test','Elite','Knight','Chaos','Hell']
 
-            try:
-                # Create SS Embed Template
-                ss_emb = discord.Embed(
-                    title=f"{emoji} {dropdown.values[0]} Scores (SS Sheet) <:SS:1041604124942270514>",
-                    color=discord.Colour.blue())
-                ss_emb.set_author(name=interaction.guild.name)
-                if interaction.guild.icon != None:
-                    ss_emb.set_author(name=interaction.guild.name, icon_url=interaction.guild.icon.url)
-                ss_emb.set_thumbnail(url=thb)
-                ss_emb.set_footer(text=interaction.user, icon_url=interaction.user.display_avatar.url)
-                ss_emb.timestamp = datetime.datetime.now()
+            # try:
+            #     # Create SS Embed Template
+            #     ss_emb = discord.Embed(
+            #         title=f"{emoji} {dropdown.values[0]} Scores (SS Sheet) <:SS:1041604124942270514>",
+            #         color=discord.Colour.blue())
+            #     ss_emb.set_author(name=interaction.guild.name)
+            #     if interaction.guild.icon != None:
+            #         ss_emb.set_author(name=interaction.guild.name, icon_url=interaction.guild.icon.url)
+            #     ss_emb.set_thumbnail(url=thb)
+            #     ss_emb.set_footer(text=interaction.user, icon_url=interaction.user.display_avatar.url)
+            #     ss_emb.timestamp = datetime.datetime.now()
 
-                # Get score data from the SS sheet
-                ssws = None
-                for a in aliases:
-                    try:
-                        ssws = self.ss_sh.worksheet(a)
-                        break
-                    except WorksheetNotFound:
-                        continue
+            #     # Get score data from the SS sheet
+            #     ssws = None
+            #     for a in aliases:
+            #         try:
+            #             ssws = self.ss_sh.worksheet(a)
+            #             break
+            #         except WorksheetNotFound:
+            #             continue
 
-                scores = ssws.batch_get(['C2:E10'], value_render_option=ValueRenderOption.formula)[0]
-                c = 0
-                for s in scores:
-                    # example data: [[20262, '', '=HYPERLINK("https://youtu.be/oWRo7r32nfM", "0:09")'], []]
-                    # if it's not a row divider but a score entry
-                    if len(s) != 0:
-                        score = s[0]
-                        x = re.search(r'\(\"(.*)\",\"(.*)\"\)', s[2].replace(' ', ''))
-                        link = ''
-                        time = ''
-                        if x != None:
-                            link, time = x.groups()
-                        else:
-                            time = s[2]
+            #     scores = ssws.batch_get(['C2:E10'], value_render_option=ValueRenderOption.formula)[0]
+            #     c = 0
+            #     for s in scores:
+            #         # example data: [[20262, '', '=HYPERLINK("https://youtu.be/oWRo7r32nfM", "0:09")'], []]
+            #         # if it's not a row divider but a score entry
+            #         if len(s) != 0:
+            #             score = s[0]
+            #             x = re.search(r'\(\"(.*)\",\"(.*)\"\)', s[2].replace(' ', ''))
+            #             link = ''
+            #             time = ''
+            #             if x != None:
+            #                 link, time = x.groups()
+            #             else:
+            #                 time = s[2]
 
-                        text = f'Score: **{score}**\nTime: **{time}**\n'
+            #             text = f'Score: **{score}**\nTime: **{time}**\n'
                         
-                        if link != '':
-                            text += f'**[Example]({link})**'
+            #             if link != '':
+            #                 text += f'**[Example]({link})**'
 
-                        ss_emb.add_field(name=f"__{diff[c]}__", value=text, inline=True)
-                        c += 1
-            except:
-                ss_emb.description = "An error has occured trying to show data from this sheet."
-                ss_emb.color = discord.Colour.red()
+            #             ss_emb.add_field(name=f"__{diff[c]}__", value=text, inline=True)
+            #             c += 1
+            # except:
+            #     ss_emb.description = "An error has occured trying to show data from this sheet."
+            #     ss_emb.color = discord.Colour.red()
 
             try:
                 # Create SSS Embed Template
@@ -453,7 +453,7 @@ class PPC(commands.Cog):
                 
                 if ranges != '':
                     url = f'https://sheets.googleapis.com/v4/spreadsheets/1p3-_Bqp4NEpqEVEFUqthxeVlvwu5uXzJSoHa8ZoN5yk?{ranges}fields=sheets(data(rowData(values(hyperlink))))'
-                    res = requests.get(url, headers={"Authorization": "Bearer " + self.gc.auth.token})
+                    res = requests.get(url, headers={"Authorization": "Bearer " + self.gc.http_client.auth.token})
                     links = res.json()['sheets'][0]['data']
                     for i in range(len(links)):
                         if len(links[i]) == 0:
@@ -500,7 +500,8 @@ class PPC(commands.Cog):
                             c += 1
 
                         sss_emb.add_field(name=f"__{diff[i]}__", value=text, inline=True)
-            except:
+            except Exception as e:
+                print(e)
                 sss_emb.description = "An error has occured trying to show data from this sheet."
                 sss_emb.color = discord.Colour.red()
 
@@ -543,7 +544,7 @@ class PPC(commands.Cog):
                 
                 if ranges != '':
                     url = f'https://sheets.googleapis.com/v4/spreadsheets/1YzOGbhTKaGTzfGbQJDdI6PSw8lXxcDpEQeeVLF4u2Dw?{ranges}fields=sheets(data(rowData(values(hyperlink))))'
-                    res = requests.get(url, headers={"Authorization": "Bearer " + self.gc.auth.token})
+                    res = requests.get(url, headers={"Authorization": "Bearer " + self.gc.http_client.auth.token})
                     links = res.json()['sheets'][0]['data']
                     for i in range(len(links)):
                         if len(links[i]) == 0:
@@ -570,18 +571,19 @@ class PPC(commands.Cog):
                                 text += f'**[Example]({links[c]})**'
                             c += 1
                         emb.add_field(name=f"__{diff[i]}__", value=text, inline=True)
-            except:
+            except Exception as e:
+                print(e)
                 emb.description = "An error has occured trying to show data from this sheet."
                 emb.color = discord.Colour.red()
             
-            self.boss_embed_cache[boss] = [ss_emb, sss_emb, emb]
+            self.boss_embed_cache[boss] = [sss_emb, emb]
             self.boss_time_created_cache[boss] = datetime.datetime.now()
 
         # Button for pagination
         if skip:
             last_cached = (datetime.datetime.now() - self.boss_time_created_cache[boss]).total_seconds()
             
-            for e in [ss_emb, sss_emb, emb]:
+            for e in [sss_emb, emb]:
                 if e is None:
                     continue
                 e.description = f"`Scores shown are from the last {int(last_cached // 60)} min(s)\nResults will update in {int(15 - last_cached // 60)} min(s).`"
@@ -596,7 +598,7 @@ class PPC(commands.Cog):
         # Send message
         success = utl.make_embed(desc="Success!", color=discord.Colour.green())
         await interaction.edit_original_response(embed=success, view=None)
-        await interaction.followup.send(embed=ss_emb, view=pagination)
+        await interaction.followup.send(embed=sss_emb, view=pagination)
     
     @app_commands.command(name='exppc_link')
     async def exppc_link(self, interaction: discord.Interaction) -> None:
@@ -659,33 +661,33 @@ class PPC(commands.Cog):
                         aliases = b['aliases']
                 records = []
 
-                # Get score data from the SS sheet
-                current_rank = "SS"
-                ssws = None
-                for a in aliases:
-                    try:
-                        ssws = self.ss_sh.worksheet(a)
-                        break
-                    except WorksheetNotFound:
-                        continue
+                # # Get score data from the SS sheet
+                # current_rank = "SS"
+                # ssws = None
+                # for a in aliases:
+                #     try:
+                #         ssws = self.ss_sh.worksheet(a)
+                #         break
+                #     except WorksheetNotFound:
+                #         continue
 
-                scores = ssws.batch_get(['C2:E10'], value_render_option=ValueRenderOption.formula)[0]
-                c = 0
-                for s in scores:
-                    # example data: [[20262, '', '=HYPERLINK("https://youtu.be/oWRo7r32nfM", "0:09")'], []]
-                    # if it's not a row divider but a score entry
-                    if len(s) != 0:
-                        score = s[0]
-                        x = re.search(r'\(\"(.*)\",\"(.*)\"\)', s[2].replace(' ', ''))
-                        time = ''
-                        if x != None:
-                            time = x.groups()[1]
-                        else:
-                            time = s[2]
+                # scores = ssws.batch_get(['C2:E10'], value_render_option=ValueRenderOption.formula)[0]
+                # c = 0
+                # for s in scores:
+                #     # example data: [[20262, '', '=HYPERLINK("https://youtu.be/oWRo7r32nfM", "0:09")'], []]
+                #     # if it's not a row divider but a score entry
+                #     if len(s) != 0:
+                #         score = s[0]
+                #         x = re.search(r'\(\"(.*)\",\"(.*)\"\)', s[2].replace(' ', ''))
+                #         time = ''
+                #         if x != None:
+                #             time = x.groups()[1]
+                #         else:
+                #             time = s[2]
 
-                        time = int((datetime.datetime.strptime(time, '%M:%S') - datetime.datetime(1900,1,1)).total_seconds())
-                        records.append({'boss':boss, 'start_of_week':start_of_week, 'rank':'SS', 'diff':diff[c], 'time':time, 'score':score})
-                        c += 1
+                #         time = int((datetime.datetime.strptime(time, '%M:%S') - datetime.datetime(1900,1,1)).total_seconds())
+                #         records.append({'boss':boss, 'start_of_week':start_of_week, 'rank':'SS', 'diff':diff[c], 'time':time, 'score':score})
+                #         c += 1
 
                 # Get score data from the SSS sheet
                 # aliases[0] should always be the name of boss from SS spreadsheet and [1] is from Whale and [2] is for SSS
@@ -795,32 +797,32 @@ class PPC(commands.Cog):
                 records = []
 
                 # Get score data from the SS sheet
-                current_rank = "SS"
-                ssws = None
-                for a in aliases:
-                    try:
-                        ssws = self.ss_sh.worksheet(a)
-                        break
-                    except WorksheetNotFound:
-                        continue
+                # current_rank = "SS"
+                # ssws = None
+                # for a in aliases:
+                #     try:
+                #         ssws = self.ss_sh.worksheet(a)
+                #         break
+                #     except WorksheetNotFound:
+                #         continue
 
-                scores = ssws.batch_get(['C2:E10'], value_render_option=ValueRenderOption.formula)[0]
-                c = 0
-                for s in scores:
-                    # example data: [[20262, '', '=HYPERLINK("https://youtu.be/oWRo7r32nfM", "0:09")'], []]
-                    # if it's not a row divider but a score entry
-                    if len(s) != 0:
-                        score = s[0]
-                        x = re.search(r'\(\"(.*)\",\"(.*)\"\)', s[2].replace(' ', ''))
-                        time = ''
-                        if x != None:
-                            time = x.groups()[1]
-                        else:
-                            time = s[2]
+                # scores = ssws.batch_get(['C2:E10'], value_render_option=ValueRenderOption.formula)[0]
+                # c = 0
+                # for s in scores:
+                #     # example data: [[20262, '', '=HYPERLINK("https://youtu.be/oWRo7r32nfM", "0:09")'], []]
+                #     # if it's not a row divider but a score entry
+                #     if len(s) != 0:
+                #         score = s[0]
+                #         x = re.search(r'\(\"(.*)\",\"(.*)\"\)', s[2].replace(' ', ''))
+                #         time = ''
+                #         if x != None:
+                #             time = x.groups()[1]
+                #         else:
+                #             time = s[2]
 
-                        time = int((datetime.datetime.strptime(time, '%M:%S') - datetime.datetime(1900,1,1)).total_seconds())
-                        records.append({'boss':boss, 'start_of_week':start_of_week, 'rank':'SS', 'diff':diff[c], 'time':time, 'score':score})
-                        c += 1
+                #         time = int((datetime.datetime.strptime(time, '%M:%S') - datetime.datetime(1900,1,1)).total_seconds())
+                #         records.append({'boss':boss, 'start_of_week':start_of_week, 'rank':'SS', 'diff':diff[c], 'time':time, 'score':score})
+                #         c += 1
 
                 # Get score data from the SSS sheet
                 # aliases[0] should always be the name of boss from SS spreadsheet and [1] is from Whale and [2] is for SSS
